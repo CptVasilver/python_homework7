14dd8a06c00d57069035d3bd579ad675eaa984a8
Mikhail Vasiliev <m89154280096@yandex.ru>
2024-02-28 22:29:15 +0300

    Homework 7 done

--- test_files.py
+++ test_files.py
@@ -28,5 +28,6 @@ def test_xlsx_file():
         with zip_file.open('file_example_XLSX_50.xlsx') as xlsx_file:
             workbook = load_workbook(xlsx_file)
             sheet = workbook.active
+
             assert workbook.sheetnames[0] == "Sheet1"
             assert sheet.cell(row=14, column=2).value == "Sherron"
